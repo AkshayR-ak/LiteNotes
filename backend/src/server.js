f7860063ace3da412d2b4bb1839be2e8d7aa98b2
akshayR-14 <akshayr5665@gmail.com>
2026-05-31 14:36:56 +0530

    updated axios /api 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json()); //to parse values of body to json
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://litenotes-backend.onrender.com',
+  'https://litenotes-backend.onrender.com/api',
   'https://lite-notes-2j5b.vercel.app/api'
 ];
 
