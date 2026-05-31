046b7f591823d29f4aeee06cda0bf998e4949d48
akshayR-14 <akshayr5665@gmail.com>
2026-05-31 13:44:47 +0530

    updated axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.NODE_ENV === "production"
   ? process.env.REACT_APP_API_URL
-  : "https://litenotes-backend.onrender.com/";
+  : "https://litenotes-backend.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
