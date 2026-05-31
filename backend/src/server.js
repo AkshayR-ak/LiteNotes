3e31efe4ec1caf0433560bde417e10cfd443663a
akshayR-14 <akshayr5665@gmail.com>
2026-05-31 13:30:58 +0530

    updated cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,8 @@ app.use(express.json()); //to parse values of body to json
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://litenotes-backend.onrender.com'
+  'https://litenotes-backend.onrender.com',
+  'https://lite-notes-2j5b.vercel.app/'
 ];
 
 const corsOptions = {
